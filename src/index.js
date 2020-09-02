1cea98c10613a2b44c28a2c983696d13c41eb7e3
utavegu <utavegu@list.ru>
2020-09-02 13:16:11 +0500

    Выполнил домашнее задание по созданию компонента на основе класса

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+const root = document.getElementById('root');
+const template = (
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>
+)
+const message = () => {
+  alert('Реакт-компонент отрендерен!');
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(template, root, message);
